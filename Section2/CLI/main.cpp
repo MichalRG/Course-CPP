925a4e30c326592e42db6ecbbaeee2432c9344d1
Michal <krzyzowskim1998@gmail.com>
2024-06-01 19:44:52 +0200

    FEAT: section 2 completed

---
+++ Section2/CLI/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello windows CLI!" << std::endl;
+	return 0;
+}
---
+++ Section2/Workspace2/Project2/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    cout << "Hello From Project II!" <<endl;
+    return 0;
+}
