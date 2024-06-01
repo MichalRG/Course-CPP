0d75b645b93f5ca7c48b12a0338494ccb5fc0f79
Michal <krzyzowskim1998@gmail.com>
2024-06-01 20:16:19 +0200

    FEAT: started section 4 - wrote simple program which get number and display some messages

---
+++ Section4/Project1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int favourite_number;
+    
+    std::cout << "Enter your fav number between 1 and 100:";
+    
+    std::cin >> favourite_number;
+    
+    std::cout << "Nice!! That's my fav number too!" << std::endl;
+    
+    return 0;
+}
