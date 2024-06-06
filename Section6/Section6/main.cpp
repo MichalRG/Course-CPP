d5cf5ac807668ae12d7ef2ba9a8f5c8fd6131b0e
Michal <krzyzowskim1998@gmail.com>
2024-06-06 22:55:03 +0200

    FEAT: added example for const from the course

--- Section6/Section6/main.cpp
+++ Section6/Section6/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
@@ -65,5 +66,28 @@ int main () {
     cout << "int " << INT_MAX << endl;
     cout << "long long " << LONG_LONG_MAX << endl;
     
+    /***********************
+     * 
+     * Declaring constants
+     * 
+    ****************************/
+    cout << "How many rooms would you like cleans?" << endl;
+    
+    int number_of_rooms {0};
+    cin >> number_of_rooms;
+
+    const double price_per_room {30};
+    const double sales_tax {30};
+    const int estimate_expiry {30}; //days
+
+    cout << "\nEstimate for carpet cleaning service" << endl;
+    cout << "Number of rooms " << number_of_rooms;
+    cout << "Price per room $" << price_per_room << endl;
+    cout << "Cost: $" << price_per_room * number_of_rooms << endl;
+    cout << "Tax: $" << price_per_room * number_of_rooms * sales_tax  << endl;
+    cout << "====================" << endl;
+    cout << "Total estimate: $" << (price_per_room * number_of_rooms) + (price_per_room * number_of_rooms * sales_tax) << endl;
+    cout << "This esmimate is valid for " << estimate_expiry << " days"<< endl;
+
     return 0;
 }
